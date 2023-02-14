eadbdaad6c7d5e375b3127a4b129ec972230a2a1
thaind194667 <thai.nd194667@sis.hust.edu.vn>
2023-02-15 02:23:57 +0700

    show, but no img yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
     
+  resources :posts
   resources :book_reviews
   get 'users/show'
   get 'static_pages/home'
   root 'static_pages#home'
   
   devise_for :users
+  get "find/:user_id", to: "posts#find"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
